b6a59c539d8f391d729280cd280249448ca6215f
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-03 09:49:25 +0300

    add view cashing

--- analytical/api/serializers/view_serializer.py
+++ analytical/api/serializers/view_serializer.py
@@ -1,7 +1,10 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import serializers
 from analytical.models import ViewModel
 
 
+@method_decorator(cache_page(60 * 15), name='list')  # Cache the list view for 15 minutes
 class ViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ViewModel
