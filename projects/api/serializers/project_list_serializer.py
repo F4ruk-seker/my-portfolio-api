9ddd722f12971601fc787b30977527b5cd62d173
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-10 11:02:08 +0300

    add projects word count

--- projects/api/serializers/project_list_serializer.py
+++ projects/api/serializers/project_list_serializer.py
@@ -1,10 +1,11 @@
 from .project_serializer import ProjectSerializer
 from projects.models import ProjectModel
+from rest_framework import serializers
 
 
 class ProjectListSerializer(ProjectSerializer):
     class Meta:
         model = ProjectModel
-        # fields = '__all__'
-        exclude = ('context',)
+        fields = '__all__'
+        # exclude = ('context',)
 
--- projects/api/serializers/project_serializer.py
+++ projects/api/serializers/project_serializer.py
@@ -3,7 +3,14 @@ from projects.models import ProjectModel
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    word_count = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_word_count(instance):
+        return len(instance.context.split(' '))
+
     class Meta:
         model = ProjectModel
         fields = '__all__'
+
 
