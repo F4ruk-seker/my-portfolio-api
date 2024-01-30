aa5be6e58cd10b6af491815cefcca92cf09290c0
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-30 18:41:10 +0300

    over write migrations

--- tags/models.py
+++ tags/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TagCategoryModel(models.Model):
     name = models.CharField(max_length=50, default=None)
-    tags = models.ManyToManyField('TagModel', blank=True)
+    tags = models.ManyToManyField('TagModel')
 
     def __str__(self):
         return str(self.name)
@@ -11,16 +11,16 @@ class TagCategoryModel(models.Model):
 
 class TagModel(models.Model):
     category = models.ForeignKey('TagCategoryModel', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50, default='')
 
     class IconType(models.TextChoices):
         FONT_AWESOME = "1", "FA"
         IMAGE = '2', 'image'
 
-    name = models.CharField(max_length=50, default='')
     icon_type = models.CharField(
         max_length=1,
         choices=IconType.choices,
         default=IconType.FONT_AWESOME
     )
-    icon = models.TextField(help_text='when call (fa/src) use this refs', blank=True)
+    icon = models.TextField(help_text='when call (fa/src) use this refs', default=None, blank=True, null=True)
 
