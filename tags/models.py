80925096ef02510675402bafec4a41e1c21f1b0e
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-30 19:02:48 +0300

    change tag sub

--- tags/models.py
+++ tags/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TagCategoryModel(models.Model):
     name = models.CharField(max_length=50, default=None)
-    tags = models.ManyToManyField('TagModel')
+    tags = models.ManyToManyField('TagModel', blank=True)
 
     def __str__(self):
         return str(self.name)
