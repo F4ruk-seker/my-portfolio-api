80bd8998f6c36e9eccf37865115caec1b260c8d8
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-07-03 21:13:02 +0300

    Fix, missed queue

--- analytical/api/views/page_month_days.py
+++ analytical/api/views/page_month_days.py
@@ -28,8 +28,9 @@ class PageMonthDaysView(APIView):
             for _ in range(1, 31):
                 for day in views_per_day_jan_2024:
                     if _ == day.get('day'):
+                        print(_)
                         result = f'_={_} / (len(apx) * 7) = {(len(apx) * 7)}'
-                        week[_ - (len(apx) * 7)] = day.get('count')
+                        week[(_ - (len(apx) * 7))-1] = day.get('count')
                         break
                 if _ % 7 == 0:
                     apx.append({
