f786cd1b41043f19af954cc39aba5dc1a08a38f0
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-08-19 14:22:41 +0300

    add Visitors result pagination

--- analytical/api/views/page_visitors_list_view.py
+++ analytical/api/views/page_visitors_list_view.py
@@ -1,8 +1,16 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+
 from analytical.api.serializers import ViewSerializer
 from pages.models import PageModel
 from django.shortcuts import get_object_or_404
+
+
+class VisitorsResultsPageNumberPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'results_size'
+    max_page_size = 100
 
 
 class PageVisitorsListView(ListAPIView):
@@ -11,6 +19,7 @@ class PageVisitorsListView(ListAPIView):
     filter_backends: list = SearchFilter, OrderingFilter
     search_fields: list = 'user_agent', 'ip_address', 'user_agent'
     ordering_fields: list = 'visit_time', 'reload_count_in_a_clock'
+    pagination_class = VisitorsResultsPageNumberPagination
 
     def get(self, request, *args, **kwargs):
         page_model: PageModel = get_object_or_404(PageModel, **{self.lookup_field: kwargs.get(self.lookup_field)})
