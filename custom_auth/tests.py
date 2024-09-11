cda74d1c1a6ea458339e39cd100bef79d3f94ad2
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-09-11 07:11:17 +0300

    Change Test Params>test user information is now random

--- custom_auth/tests.py
+++ custom_auth/tests.py
@@ -5,29 +5,34 @@ from django.shortcuts import reverse, resolve_url
 from django.contrib.auth.models import User
 from config.settings import test
 from requests import api
+from faker import Faker
 
 
 class AuthTest(TestCase):
     def setUp(self):
-        self.bad_user = User.objects.create_user(
-            username='echo',
-            password='ratatatata01!',
-            email='f4@f4.com'
-        )
-        # self.bad_user
-        self.admin_user = User.objects.create_superuser(
-            username='admin',
-            password='adminadmin!01!',
-            email='f22@f4.com',
-        )
+        self.fake = Faker()
+        self.bad_user_credentials = {
+            'username': self.fake.user_name(),
+            'password': self.fake.password(),
+            'email': self.fake.password()
+        }
+        self.bad_user = User.objects.create_user(**self.bad_user_credentials)
+
+        self.admin_user_credentials = {
+            'username': self.fake.user_name(),
+            'password': self.fake.password(),
+            'email': self.fake.password()
+        }
+
+        self.admin_user = User.objects.create_superuser(**self.admin_user_credentials)
 
     def test_bad_user_cant_login_admin_panel(self):
-        self.client.login(username='echo', password='ratatatata01!')
+        self.client.login(**self.bad_user_credentials)
         response = self.client.get(reverse('api:custom_admin:page-manage'))
         self.assertEqual(response.status_code, 403)
 
     def test_jwt_refresh(self):
-        self.client.login(username='admin', password='adminadmin!01!')
+        self.client.login(**self.admin_user_credentials)
         test_url = f'{test.MACHINE_IP}{reverse('api:custom_admin:page-manage')}'
         login_url_token = resolve_url(reverse('api:auth:token_obtain_pair'))
         p = api.post(login_url_token, {
