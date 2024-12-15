ca34ae69be94efa471323deeffeb1abd2f6bd9dc
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-12-15 04:51:32 +0300

    new message notification service

--- message/api/serializers/message_serializer.py
+++ message/api/serializers/message_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from analytical.api.serializers import ViewSerializer
 from analytical.utils import ViewCountWithRule
 from message.models import MessageModel
+from config.settings.base import CUSTOM_LOGGER
 
 
 class MessageCreateSerializer(serializers.ModelSerializer):
@@ -12,8 +13,20 @@ class MessageCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         counter = ViewCountWithRule(None, self.context['request'])
-        validated_data['view'] = counter.create_view()
+        view = counter.create_view()
+        validated_data['view'] = view
         message = super(MessageCreateSerializer, self).create(validated_data)
+        CUSTOM_LOGGER.construct(
+            title=f'New Message From [{message.email}]',
+            level='verbose',
+            metadata={
+                "name": message.name,
+                "email": message.email,
+                "ip": view.ip_address,
+            },
+            description=f'Message: {message.message}'
+        )
+        CUSTOM_LOGGER.send()
         return message
 
 
--- message/api/views/create_message.py
+++ message/api/views/create_message.py
@@ -7,10 +7,3 @@ class CreateMessageView(CreateAPIView):
     model = MessageModel
     serializer_class = MessageCreateSerializer
     authentication_classes = []
-
-    # def create(self, request, *args, **kwargs):
-    #     counter = ViewCountWithRule(None, self.request)
-    #     sender_agent = counter.create_view()
-    #     request.data['sender_agent'] = sender_agent.id
-    #     print("redm")
-    #     return super().create(request, *args, **kwargs)
