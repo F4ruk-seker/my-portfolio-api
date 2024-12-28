3621ba324f92a6da48b8546cb35ae5b9ced5e0d2
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-12-28 05:56:16 +0300

    Allow Cloudflare

--- config/settings/product.py
+++ config/settings/product.py
@@ -112,11 +112,28 @@ sentry_sdk.init(
     send_default_pii=True
 )
 
-INTERNAL_IPS = [
-    "farukseker.com.tr",
-    "api.farukseker.com.tr",
-]
+
 INSTALLED_APPS.append('debug_toolbar')
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
+from ipaddress import ip_network
+
+CLOUDFLARE_IP_RANGES = [
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/12",
+    "172.64.0.0/13",
+    "131.0.72.0/22",
+]
+
+INTERNAL_IPS = [str(ip) for cidr in CLOUDFLARE_IP_RANGES for ip in ip_network(cidr).hosts()]
