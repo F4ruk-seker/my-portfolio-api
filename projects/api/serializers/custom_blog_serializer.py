073b0880413694c3b6873042a49f80da68a6cefc
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-01-19 00:49:36 +0300

    RM old token

--- projects/api/serializers/custom_blog_serializer.py
+++ projects/api/serializers/custom_blog_serializer.py
@@ -4,5 +4,14 @@ from .project_serializer import ContentSerializer
 
 class CustomBLogContentSerializer(serializers.Serializer):
     contents = ContentSerializer(many=True)
-    trends = ContentSerializer(many=True)
+    # trends = ContentSerializer(many=True)
+    trends = serializers.SerializerMethodField()
     featured = ContentSerializer(many=True)
+
+    @staticmethod
+    def get_trends(instance):
+        return [{'title': trend.title, 'slug': trend.slug} for trend in instance.get('trends', [])]
+
+    '''
+    @feured objeleri contente var ise silinecek | opts
+    '''
